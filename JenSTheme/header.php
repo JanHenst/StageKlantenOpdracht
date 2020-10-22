b8095dd946f8450ab8549e07a84b1104f8944ea8
JanHenst <42889823+JanHenst@users.noreply.github.com>
2020-10-22 15:21:11 +0200

    Add files via upload

--- JenSTheme/header.php
+++ JenSTheme/header.php
@@ -5,5 +5,22 @@
 </head>
 <body>
 <header>
-    <div class="logo">J&S</div> <a href="./?page_id=3">Home</a> <a href="./?page_id=3">Over ons</a> <a href="./?page_id=3">Reportoire</a>  <a href="./?page_id=3">Speellijst</a> <a href="./?page_id=3">Shop</a> <a href="./?page_id=3">Reviews</a> <a href="./?page_id=3">Contact</a> <div class="email"></div><div class="likeButton"></div>
+    <div class="nav">
+      <h2 class="logo">J&S</h2>
+       <a href="./?page_id=3">Home</a>
+       <a href="./?page_id=3">Over ons</a>
+       <a href="./?page_id=3">Reportoire</a>
+       <a href="./?page_id=3">Speellijst</a>
+       <a href="./?page_id=3">Shop</a>
+       <a href="./?page_id=3">Reviews</a>
+       <a href="./?page_id=3">Contact</a>
+      <div class="nav-icons">
+          <div class="email-nav">
+            icon
+          </div>
+          <div class="likeButton-nav">
+            icon
+          </div>
+      </div>
+    </div>
 </header>
--- JenSTheme/index.php
+++ JenSTheme/index.php
@@ -17,6 +17,19 @@ wp_head();
 get_header();
 
 add_theme_scripts();
+?>
 
+<html>
+
+<div id="page-container">
+    <div id="content-wrap">
+        <h1>Ello</h1>
+    </div>
+
+
+</html>
+
+<?php
 
 get_footer();
+
