7493fb5ec7a5deb7e92378bd61fbcffa0384eb87
florianmac <nairolf3>
2020-10-26 12:12:44 +0100

    theme options
    added theme options to customise theme no change yet

--- JenSTheme/functions.php
+++ JenSTheme/functions.php
@@ -23,36 +23,51 @@ add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
 
 
-function panel( $wp_customize )
+function colors( $wp_customize )
 {
+	$wp_customize->add_section( 'starter_new_section_name' , array(
+		'title'    => __( 'thema opties', 'starter' ),
+		'priority' => 10
+	) );
 
-    $wp_customize->add_panel('some_panel', array(
-        'title' => 'color change',
-        'description' => 'change colors',
-        'priority' => 10,
-    ));
+	$wp_customize->add_setting( 'achtergrond' , array(
+		'default'   => '#000000',
+		'transport' => 'refresh',
+	) );
+	$wp_customize->add_setting( 'text' , array(
+		'default'   => '#000000',
+		'transport' => 'refresh',
+	) );
 
+	$wp_customize->add_setting( 'accent1' , array(
+		'default'   => '#000000',
+		'transport' => 'refresh',
+	) );
 
-    $wp_customize->add_section('section', array(
-        'title' => 'section',
-        'priority' => 10,
-        'panel' => 'some_panel',
-    ));
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'Theme-achtergrond', array(
+		'label'    => __( 'achtergrond', 'starter' ),
+		'section'  => 'starter_new_section_name',
+		'settings' => 'achtergrond',
 
+	) ) );
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'Theme-text', array(
+		'label'    => __( 'text', 'starter' ),
+		'section'  => 'starter_new_section_name',
+		'settings' => 'text',
+	) ) );
 
-    $wp_customize->add_setting('setting', array(
-        'default' => 'a',
-    ));
-
-
-    $wp_customize->add_control('contrl_demo', array(
-        'label' => 'Text',
-        'type' => 'text',
-        'section' => 'section',
-        'settings' => 'setting_demo',
-    ));
-};
-    add_action('customize_register','panel');
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'Theme-accent1', array(
+		'label'    => __( 'accent 1', 'starter' ),
+		'section'  => 'starter_new_section_name',
+		'settings' => 'text',
+	) ) );
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'Theme-accent2', array(
+		'label'    => __( 'accent 2', 'starter' ),
+		'section'  => 'starter_new_section_name',
+		'settings' => 'text',
+	) ) );
+}
+add_action('customize_register','colors');
 
 
 
