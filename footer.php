232929036d21afe46c7aa43f8d60959893283c97
JanHenst <42889823+JanHenst@users.noreply.github.com>
2020-10-29 16:46:29 +0100

    Add files via upload
    Update footer + nav zodat ze sticky zijn

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+      <footer class="footer">
+        &copy;2020 Jenneke & Sanne
+      </footer>
+
+    </div>
+  </body>
+</html>
+
+
+
+
+
---
+++ javascript/header/hamburger.js
@@ -0,0 +1,37 @@
+let hamburger = document.getElementsByClassName('hamburger')[0];
+var buttons = document.getElementsByClassName('headerbutton');
+var icons =  document.getElementsByClassName('nav-icons');
+var pannel =  document.createElement("div");
+var PannelBool = true;
+
+
+const button_amount = buttons.length;
+for(i=0;i<button_amount;i++) {
+
+    pannel.className="pannel";
+    pannel.classList.add('no-pannel');
+    pannel.appendChild(buttons[0]);
+
+}
+pannel.appendChild(icons[0]);
+
+function panelMake() {
+
+    document.body.appendChild(pannel);
+}
+
+hamburger.addEventListener('click',function () {
+
+
+
+    if(PannelBool===true) {
+        pannel.classList.remove('no-pannel');
+        PannelBool=false;
+    }
+    else if(PannelBool===false){
+        pannel.classList.add('no-pannel');
+        PannelBool=true;
+    }
+    console.log("Pannel is"+PannelBool);
+
+});
