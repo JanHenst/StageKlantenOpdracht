952e24aa816b96bf74d201077bd7dc6d50d83f45
JanHenst <42889823+JanHenst@users.noreply.github.com>
2020-11-05 14:23:36 +0100

    Add files via upload

--- JenSTheme/header.php
+++ JenSTheme/header.php
@@ -10,27 +10,27 @@
         <svg id="hamburger-icon" class="hamburger menu-icon">
             <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/header/menu.svg#menu'></use>
         </svg>
-      <h2 class="logo"><a href="">J&S</a></h2>
-    <?php
-    $pages = get_pages();
-//    echo "<a class='headerbutton' href='./'>home</a>";
-    for($i=0;$i<sizeof($pages);$i++){
-        if(get_the_title($pages[$i])=="404"){
-            $i++;
+        <h2 class="logo"><a href="">J&S</a></h2>
+        <?php
+        $pages = get_pages();
+        //    echo "<a class='headerbutton' href='./'>home</a>";
+        for($i=0;$i<sizeof($pages);$i++){
+            if(get_the_title($pages[$i])=="404"){
+                $i++;
+            }
+            else {
+                echo"<a class='headerbutton' href='".get_page_link($pages[$i])."'>".get_the_title( $pages[ $i ] )."</a>";
+            }
         }
-        else {
-	        echo"<a class='headerbutton' href='".get_page_link($pages[$i])."'>".get_the_title( $pages[ $i ] )."</a>";
-        }
-    }
 
-?>
-      <div class="nav-icons">
-          <svg class="email-nav">
-              <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/header/email-icon.svg#email'></use>
-          </svg>
-          <svg class="facebook-nav">
-              <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/header/facebook.svg#facebook'></use>
-          </svg>
-      </div>
+        ?>
+        <div class="nav-icons">
+            <svg class="email-nav">
+                <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/header/email-icon.svg#email'></use>
+            </svg>
+            <svg class="facebook-nav">
+                <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/header/facebook.svg#facebook'></use>
+            </svg>
+        </div>
     </div>
 </header>
--- JenSTheme/index.php
+++ JenSTheme/index.php
@@ -19,12 +19,21 @@ get_header();
 add_theme_scripts();
 ?>
 <div class="carouselContainer">
-    <div class="arrowL"><</div>
-    <div class="arrowR">></div>
+    <div class="arrowL">
+        <svg id="arrow_left" class="arrow-carousel">
+            <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/carousel/arrow_left.svg#left-arrow'></use>
+        </svg>
+    </div>
+    <div class="arrowR">
+        <svg id="arrow_right" class="arrow-carousel">
+            <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/carousel/arrow_right.svg#right-arrow'></use>
+        </svg>
+    </div>
     <div class="carousel"></div>
 
 
 </div>
+<div class="main">
 <?php
 $post= get_post(2);
 echo '<h1 class="titel">'.get_the_title($post).'</h1>';
@@ -33,7 +42,7 @@ echo explode("</p>",get_the_content())[0];
 echo "<a class='link' href='".get_page_link($post)."'>Lees meer...</a>";
 ?>
 
-
+</div>
 
 <?php
 
