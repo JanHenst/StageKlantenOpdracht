e6319eb332040818539c5d3b80fdd392d44f689e
Jan van der Henst <26615@ma-web.nl>
2020-11-30 15:20:16 +0100

    Contact pagina
    Ik heb de Contact pagina gemaakt.

---
+++ JenSTheme/contact_template.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Template Name: Contact
+*/
+wp_head();
+
+
+get_header();
+include_once( 'carousel.php' );
+
+add_theme_scripts();
+?>
+<div class="main">
+    <?php get_header() ?>
+
+    <div id="container">
+        <div id="content">
+            <?php the_post() ?>
+            <div id="post-<?php the_ID() ?>" class="post">
+                <div class="entry-content">
+                    <?php echo "<div> <h1>" .get_the_title(get_post()) . "</h1> </div>";?>
+                    <?php the_content(); ?>
+                    <div class="wp-block-column form-contact" style="flex-basis: 66.66%;">
+
+                    <form action="<?php the_permalink(); ?>" id="contactForm" method="post">
+                        <ul>
+                            <div class="flex-inputs">
+                                <li class="name-input">
+                                    <input type="text" name="contactName" id="contactName" value="" placeholder="Name"/>
+                                </li>
+                                <li class="tel-input">
+                                    <input type="text" name="contactName" id="contactName" value="" placeholder="Tel"/>
+                                </li>
+                            </div>
+                            <li>
+                                <input class="email-input" type="text" name="email" id="email" value="" placeholder="Email"/>
+                            </li>
+                            <li>
+                                <textarea name="comments" id="commentsText" rows="10" cols="44" placeholder="Message"></textarea>
+                            </li>
+                            <li>
+                                <button class="button-form" type="submit">Submit</button>
+                            </li>
+                        </ul>
+                        <input type="hidden" name="submitted" id="submitted" value="true" />
+                    </form>
+                    </div>
+                    </div>
+                </div><!-- .entry-content -->
+            </div><!-- .post-->
+        </div><!-- #content -->
+    </div><!-- #container -->
+
+</div>
+
+<?php
+
+get_footer();
+wp_footer();
+
+?>
