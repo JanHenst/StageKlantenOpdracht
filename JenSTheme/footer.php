7ceea40868e20747aa880c3042f818758ae99f98
JanHenst <42889823+JanHenst@users.noreply.github.com>
2021-01-14 14:45:40 +0100

    fixed footer en carousel

--- JenSTheme/footer.php
+++ JenSTheme/footer.php
@@ -1,14 +1,25 @@
 
       <footer class="footer">
           <h3>Jenneke & Sanne</h3>
-          <div class="contactInfo">
-              <h3>Contact</h3>
-              <a target=”_blank” href="mailto:jennekeensanne@gmail.com">jennekeensanne@gmail.com</a>
-              <p></p>
-              &copy;2020 Jenneke & Sanne
-          </div><a target=”_blank” href="https://facebook.com/liedjesduo.nl/"><svg class="facebook-footer">
-              <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/header/facebook.svg#facebook'></use>
-          </svg><a/>
+          <div class="footer-content">
+              <div class="contactInfo">
+                  <h3>Contact</h3>
+                  <a target=”_blank” href="mailto:jennekeensanne@gmail.com">jennekeensanne@gmail.com</a>
+                  <p>
+                    &copy;2020 Jenneke & Sanne
+                  </p>
+              </div>
+              <div class="facebook-link">
+                  <a target=”_blank” href="https://facebook.com/liedjesduo.nl/">
+                      <svg class="facebook-footer">
+                        <use xlink:href='<?php echo get_template_directory_uri()?>/plaatjes/header/facebook.svg#facebook'></use>
+                      </svg>
+                  <a/>
+              </div>
+              <p class="telefoon-text-footer">
+                  &copy;2020 Jenneke & Sanne
+              </p>
+          </div>
       </footer>
 
     </div>
--- JenSTheme/header.php
+++ JenSTheme/header.php
@@ -3,7 +3,7 @@
 
 <head>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1"></head>
 <body onload="panelMake()">
